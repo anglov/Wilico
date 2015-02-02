162d588f73764cf4d40a8be61387cfd098faaa41
Darkkonan <kamcio_921011@interia.pl>
2015-02-02 21:23:22 +0100

    Finder new functionality
    Added support for command-line arguments,
    Improved print of results,
    Added results count limiter,
    Added possibility to change base

--- Finder.py
+++ Finder.py
@@ -1,26 +1,52 @@
 # -*- coding: utf-8 -*-
 import csv
+import argparse
 
 
 def load_data(path):
     data = []
     with open(path, encoding="utf-8") as file:
-       r = csv.reader(file,
+        r = csv.reader(file,
             dialect=csv.unix_dialect)
-       for row in r:
+        for row in r:
             data.append([row[0], int(row[1])])
-            # print ([row[0], int(row[1])])
     return data
 
 
-def suggester(input, data):
+def suggester(word, data, exactly):
     result = []
     for item in data:
-        if item[0].find(input)>-1:
-            result.append(item)
+        if exactly:
+            if item[0] == word:
+                result.append(item)
+        else:
+            if item[0].find(word) > -1:
+                result.append(item)
     return sorted(result, reverse=True, key=lambda y: y[1])
 
 
+def print_results(results, number):
+    print("Counts\t Title")
+    for n, l in enumerate(results):
+        if n >= number:
+            break
+        print(str(l[1])+'\t\t'+l[0])
+
+
 if __name__ == '__main__':
-    test = load_data("C:/wilico/count.csv")
-    print (suggester("Fort",test))
+
+    parser = argparse.ArgumentParser(description='Finding word in base of titles of wikipedia and show links to this site')
+    parser.add_argument('Words', metavar='Words', type=str, nargs='+',
+                        help='Words to find in base')
+    parser.add_argument('--base', default='count.csv', type=str,
+                        help='directory od the base (default /count.csv)')
+    parser.add_argument('--no', type=int, default=10,
+                        help='numer of showed links in output ( default 10 )')
+    parser.add_argument('--exa', action='store_const',
+                        const=True, default=False,
+                        help='exactly phase (default is finding all titles containing that phase)')
+    args = parser.parse_args()
+    base = load_data(args.base)
+    for w in args.Words:
+        print('Results for '+w)
+        print_results(suggester(w, base, args.exa), args.no)
