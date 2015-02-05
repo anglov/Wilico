7e56f39d301f93e85ec92efef5fb562b522ca968
anglov <anglov92@gmail.com>
2015-02-05 06:22:40 +0100

    Add possible to pass many input files

--- Counter.py
+++ Counter.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 import csv
 import argparse
 import operator
-from multiprocessing import Queue, Process
+from multiprocessing import Queue, Process, Lock, Value
 from queue import Empty
 
 
@@ -15,25 +15,36 @@ class Counter():
         while True:
             item = q_in.get()
             if item == Empty:
-                q_in.put(Empty)
-                q_out.put(Empty)
-                break
+                with self.parsers_done_lock:
+                    self.parsers_done.value += 1
+                    print(self.parsers_done.value)
+                    if self.parsers_done.value >= self.parsers:
+                        q_in.put(Empty)
+                        q_out.put(Empty)
+                        break
+                    else:
+                        continue
             q_out.put(self._get_links(item))
 
-    def _reader(self, IN, q_out):
-        if IN.endswith("bz2"):
-            open_func = bz2.open
-        else:
-            open_func = open
-        with open_func(IN) as f:
-            context = ET.iterparse(f, events=("start", "end"))
-            event, root = context.__next__()
-            for event, elem in context:
-                if event == "end" and elem.tag == "text":
-                    if elem.text:
-                        q_out.put(elem.text)
-                    root.clear()
-            q_out.put(Empty)
+    def _reader(self, file_in, q_out):
+        while True:
+            path = file_in.get()
+            if path == Empty:
+                file_in.put(Empty)
+                q_out.put(Empty)
+                break
+            if path.endswith("bz2"):
+                open_func = bz2.open
+            else:
+                open_func = open
+            with open_func(path) as f:
+                context = ET.iterparse(f, events=("start", "end"))
+                event, root = context.__next__()
+                for event, elem in context:
+                    if event == "end" and elem.tag == "text":
+                        if elem.text:
+                            q_out.put(elem.text)
+                        root.clear()
 
     def _get_links(self, page_contents):
         page = io.StringIO()
@@ -43,30 +54,43 @@ class Counter():
         pa = re.findall("\[\[([^\[\]\|\#]+)(?:[|#][^\]]+)?\]\]", page, flags=re.UNICODE)
         return pa
 
-    def links_counter(self, IN):
+    def links_counter(self, files):
         links_count = defaultdict(lambda: 0)
         end_count = 0
-        q_in, q_out = Queue(), Queue()
+        file_out, q_in, q_out = Queue(), Queue(), Queue()
         processes = []
-        p = Process(target=self._reader, args=(IN, q_out))
-        p.start()
-        for ii in range(self.children):
+        readers = []
+
+        for _ in range(self.parsers):
+            p = Process(target=self._reader, args=(file_out, q_out))
+            p.start()
+            readers.append(p)
+
+        for _ in range(self.workers):
             p = Process(target=self._process, args=(q_in, q_out))
             p.start()
             processes.append(p)
+
+        for file in files:
+            file_out.put(file)
+        file_out.put(Empty)
+
         while True:
             item = q_in.get()
             if item == Empty:
                 end_count += 1
-                if end_count >= self.children:
+                if end_count >= self.workers:
                     break
                 else:
                     continue
             for link in item:
                 links_count[link.strip()] += 1
-        p.join()
+
+        for p in readers:
+            p.join()
         for p in processes:
             p.join()
+
         return links_count
 
     def save_to_file(self, out_file, contents):
@@ -75,20 +99,26 @@ class Counter():
             for item in sorted(contents.items(), key=operator.itemgetter(0)):
                 w.writerow([item[0],item[1]])
 
-    def __init__(self, children):
-        self.children = children
+    def __init__(self, workers, parsers):
+        self.workers = workers
+        self.parsers = parsers
+        self.parsers_done = Value("i",0)
+        self.parsers_done_lock = Lock()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Counting links from wikipedia')
-    parser.add_argument('Input', metavar='Input', type=str, nargs='?',
+    parser.add_argument('Input', metavar='Input', type=str, nargs='+',
                         help='file with  in format xml packed in .bz2')
     parser.add_argument('--out', default='count.csv', type=str,
                         help='directory od the output (default /count.csv)')
     parser.add_argument('--workers', default=8, type=int,
                         help='Number of workers, that will be spawn')
+    parser.add_argument('--parsers', default=2, type=int,
+                        help='Number of file parsers, that will be spawn.')
     args = parser.parse_args()
-    children = args.workers
-    counter = Counter(children)
+    workers = args.workers
+    parsers = args.parsers
+    counter = Counter(workers, parsers)
     result = counter.links_counter(args.Input)
     counter.save_to_file(args.out ,result)
